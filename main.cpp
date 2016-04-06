297dbba690aa444a1864b274ead7f7af704b8215
Jackson Bockus <jackson.bockus@gmail.com>
2016-04-06 17:49:59 -0500

    Added main.cpp, which contains main method for execution.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// Used for getting parameters from the console
+#include <iostream>
+#include "IceField.hpp"
+
+int main()
+{
+    // The input parameters, in the order in which they're expected.
+    int rows;
+    int columns;
+    int startingRow;
+    int startingColumn;
+    int numSteps;
+    
+    // Gathers input in the specified order.
+    std::cin >> rows;
+    std::cin >> columns;
+    std::cin >> startingRow;
+    std::cin >> startingColumn;
+    std::cin >> numSteps;
+    
+    // The starting row and column numbers are unit-indexed,
+    // but zero-indexed numbers are easier to use, so
+    --startingRow;
+    --startingColumn;
+    
+    IceField rink(rows, columns, startingRow, startingColumn, int numSteps);
+    rink.clean();
+    std::cout << rink << endl;
+}
